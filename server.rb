e51c3bca257a8663c139bc8f339a7e01047be759
D4l3k <rice@outerearth.net>
2011-12-18 20:47:47 -0800

    Debugs

--- server.rb
+++ server.rb
@@ -5,8 +5,7 @@ class Server
 	attr_accessor :c_socket, :s_socket
 	def initialize
 		puts "Connecting to client"
-		@c_socket = EventMachine.connect 'mc.outerearth.net', 25564, Connection do |con|
-			con.client = true
+		@c_socket = EventMachine.connect 'mc.outerearth.net', 25564, Connection, true
 		end
 		@s_socket = false
 	end
@@ -14,6 +13,9 @@ end
 
 class Connection < EM::Connection
 	attr_accessor :client
+	def initialize client
+		@client = client
+	end
 	def post_init
 		puts "Connected. Client: #{@client}"
 	end
@@ -21,9 +23,7 @@ class Connection < EM::Connection
 		if @client
 			if !@@server.s_socket
 				put "Connecting to server"
-				@@server.s_socket = EventMachine.connect 'localhost',25565, Connection do |con|
-                		        con.client = false
-		                end
+				@@server.s_socket = EventMachine.connect 'localhost',25565, Connection, false
 			end
 
 			@@server.s_socket.send_data data
