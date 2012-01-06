bcbc3567b5f760ba86c6921945bb0e10ba8efde9
D4l3k <rice@outerearth.net>
2012-01-05 19:15:19 -0800

    Rewrote to handle multiple clients.

--- client.rb
+++ client.rb
@@ -1,36 +1,75 @@
 require 'rubygems'
 require 'eventmachine'
 
-@@server = false
-@@client = false
+$control = false
+$clients = []
+$waiting_clients = []
 
 class Connection < EM::Connection
-    def initialize
-	    if !$server
-		    $server = self
-		    @s = true
-		    puts "Server connected!"
-	    elsif
-		    $client = self
-		    @s = false
-		    puts "Client connected!"
-	    end
-    end
-    def receive_data(data)
-	    if !@s
-		    $server.send_data data
-		    puts "to server"
-	    else
-		    $client.send_data data
-		    puts "to client"
-	    end
-	    puts data
-    end
-    def unbind
-    	puts "Lost connection. server: #{@s}"
-    end
+attr_accessor :queue, :server_connection
+	def initialize
+    		port, *ip_parts = get_peername[2,6].unpack "nC4"
+		ip = ip_parts.join('.')
+		puts "Client Connection from: #{ip}."
+		$clients.push self
+		$waiting_clients.push self
+		@server_connection = false
+		$control.demand_new_connection
+    	end
+    	def receive_data(data)
+		if !@server_connection
+			@server_connection.send_data data
+		else
+			@queue.push data
+		end
+    	end
+    	def unbind
+    		puts "Lost connection from client. Closing Server connection."
+		@server_connection.close_connection_after_writing
+    	end
+end
+class ServerConnection < EM::Connection
+attr_accessor :client_connection
+	def initialize
+		port, *ip_parts = get_peername[2,6].unpack "nC4"
+		ip = ip_parts.join('.')
+		puts "Server Connection from: #{ip}."
+		@client_connection = $waiting_clients.pop(0)
+		if pop == nil
+			puts "No waiting connections"
+			close_connection_after_writing
+		else
+			puts "Attaching to connection."
+			@client_connection.server_connection = self
+			puts "Sending queue."
+			while (data = @client_connection.queue.pop(0))!=nil
+				send_data data
+			end
+		end
+	end
+	def receive_data(data)
+		@client_connection.send_data data
+	end
+	def unbind
+    		puts "Lost connection from server. Closing client connection."
+		@client_connection.close_connection_after_writing
+    	end
+end
+class ControlConnection < EM::Connection
+	def initialize
+		$control = self
+
+		port, *ip_parts = get_peername[2,6].unpack "nC4"
+		ip = ip_parts.join('.')
+		puts "Gained Control Connection from: #{ip}."
+	end
+	def demand_new_connection
+		send_data "new_connection"
+	end
 end
 
 EventMachine.run {
     EventMachine::start_server "0.0.0.0", 25564, Connection
+    EventMachine::start_server "0.0.0.0", 25563, ServerConnection
+    EventMachine::start_server "0.0.0.0", 25562, ControlConnection
 }
--- server.rb
+++ server.rb
@@ -1,41 +1,53 @@
 require 'rubygems'
 require 'eventmachine'
-		
-class Server
-	def initialize
-		puts "Connecting to client"
-		$c_socket = EventMachine.connect 'mc.outerearth.net', 25564, ServConnection, true
-		$first_run = true
-	end
-end
 
-class ServConnection < EventMachine::Connection
-	attr_accessor :client
-	def initialize client
-		@client = client
-	end
-	def post_init
-		puts "Connected. Client: #{@client}"
+$control = false
+$clients = []
+
+class ClientConnection < EventMachine::Connection
+	attr_accessor :server_connection
+	def initialize
+		@first_run = true
+		$clients.push self
+		puts "Client connection."
 	end
 	def receive_data data
-		puts "Recieved data"
-		if @client
-			"from client"
-			if $first_run
-				puts "First Run!"
-				$first_run = false
-				$s_socket = EventMachine.connect 'localhost',25565, ServConnection, false
-			end
-			$s_socket.send_data data
-			puts "To S: #{data}"
-		else
-			$c_socket.send_data data
-			puts "To C: #{data}"
+		if @first_run
+			@server_connection = EventMachine.connect 'localhost', 25565, ServerConnection
+			@server_connection.client_connection = self
+		end
+		@server_connection.send_data data
+	end
+	def unbind
+		puts "Lost connection from client. Closing Server connection."
+		@server_connection.close_connection_after_writing
+	end
+end
+class ServerConnection < EventMachine::Connection
+	attr_accessor :client_connection
+	def initialize
+		puts "New server connection."
+	end
+	def receive_data data
+		@client_connection.send_data data
+	end
+	def unbind
+		puts "Lost server connection. Closing client connection."
+		@client_connection.close_connection_after_writing
+	end
+end
+class ControlConnection
+	def initialize
+		$control = self
+	end
+	def receive_data data
+		if data="new_connection"
+			EventMachine.connect 'mc.outerearth.net', 25563, ClientConnection
 		end
 	end
 	def unbind
 	end
 end
 EventMachine.run {
-	$server = Server.new
+	EventMachine.connect 'mc.outerearth.net', 25562, ControlConnection
 }
